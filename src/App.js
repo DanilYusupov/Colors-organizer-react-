7ad610540519fd935ed515c72901dcac37e3dba3
Yusupov Danil <danil.usupov@icl-services.com>
2018-09-26 11:21:25 +0300

    Added AddColorForm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AddColorForm from './components/AddColorForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <AddColorForm onNewColor={(title, color) => {
+              console.log('TODO: add new ${title} and ${color} to the list')
+          }}/>
       </div>
     );
   }
