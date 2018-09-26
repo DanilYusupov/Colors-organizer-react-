1fa495f8cf93bb56d11ffdc42b1a34f62fd30eb8
Yusupov Danil <danil.usupov@icl-services.com>
2018-09-26 13:51:18 +0300

    Added delete and rating functions

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
             colors: [],
         };
         this.addColor = this.addColor.bind(this);
+        this.rateColor = this.rateColor.bind(this);
+        this.removeColor = this.removeColor.bind(this);
     }
 
     addColor(title, color) {
@@ -26,13 +28,33 @@ class App extends Component {
         this.setState({ colors })
     }
 
+    rateColor(id, rating) {
+        const colors = this.state.colors.map(color =>
+            (color.id !== id) ?
+                color :
+                {
+                    ...color,
+                    rating,
+                },
+        )
+    }
+
+    removeColor(id) {
+        const colors = this.state.colors.filter(
+            color => color.id !== id,
+        );
+        this.setState(colors);
+    }
+
     render() {
-        const { addColor } = this;
+        const { addColor, rateColor, removeColor } = this;
         const { colors } = this.state;
         return (
             <div className="App">
                 <AddColorForm onNewColor={addColor}/>
-                <ColorList colors={colors}/>
+                <ColorList colors={colors}
+                           onRate={rateColor}
+                           onRemove={removeColor}/>
             </div>
         );
     }
