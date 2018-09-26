351ebc694ec479fe3772b8910ef2783459f5c1e1
Yusupov Danil <danil.usupov@icl-services.com>
2018-09-26 13:39:47 +0300

    Added up throw in the tree

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import AddColorForm from './components/AddColorForm';
+import ColorList from './components/ColorList';
+import { v4 } from 'uuid';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <AddColorForm onNewColor={(title, color) => {
-              console.log('TODO: add new ${title} and ${color} to the list')
-          }}/>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            colors: [],
+        };
+        this.addColor = this.addColor.bind(this);
+    }
+
+    addColor(title, color) {
+        const colors = [
+            ...this.state.colors,
+            {
+                id: v4(),
+                title,
+                color,
+                rating: 0,
+            },
+        ];
+        this.setState({ colors })
+    }
+
+    render() {
+        const { addColor } = this;
+        const { colors } = this.state;
+        return (
+            <div className="App">
+                <AddColorForm onNewColor={addColor}/>
+                <ColorList colors={colors}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../css/star.css';
 
-const Star = ({ selected = false, onClick = f => f }) => {
+const Star = ({ selected = false, onClick = f => f }) =>
     <div className={(selected) ? 'star selected' : 'star'}
          onClick={onClick}>
-    </div>
-};
+    </div>;
 
 Star.propTypes = {
     selected: PropTypes.bool,
